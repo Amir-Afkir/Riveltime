92aeeeadab1b4d61db6d78125cca80e32ecac249
Amir-Afkir <afkir202@gmail.com>
2025-06-27 20:53:40 +0200

    bug9

--- src/pages/common/Profil.jsx
+++ src/pages/common/Profil.jsx
@@ -35,12 +35,8 @@ export default function ProfilCommun() {
       });
 
       if (!response.ok) throw new Error("Erreur serveur");
-      logout({
-        returnTo:
-          import.meta.env.MODE === "production"
-            ? "https://riveltime.app"
-            : "http://localhost:5173",
-      });
+
+      logout({ returnTo: window.location.origin });
     } catch (err) {
       console.error("❌ Erreur lors de la suppression :", err);
       alert("La suppression du compte a échoué.");
@@ -194,14 +190,7 @@ export default function ProfilCommun() {
             <div className="flex justify-between">
               <button
                 className="text-sm text-gray-700 hover:text-gray-900"
-                onClick={() =>
-                  logout({
-                    returnTo:
-                      import.meta.env.MODE === "production"
-                        ? "https://riveltime.app"
-                        : "http://localhost:5173",
-                  })
-                }
+                onClick={() => logout({ returnTo: window.location.origin })}
               >
                 Se déconnecter
               </button>
