2f17d956b0499048f5fe77525cd835a5b788c65e
Amir-Afkir <afkir202@gmail.com>
2025-06-27 17:52:08 +0200

    bug

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,18 +5,19 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+const { jwtCheck, injectUser, createUserIfNotExists } = require('../middleware/auth');
 const { getMyProfile, updateMyProfile, uploadAvatar, deleteAvatar } = require('../controllers/userController');
 
 // ✅ Route protégée pour récupérer son profil
-router.get('/me', getMyProfile);
+router.get('/me', jwtCheck, injectUser, createUserIfNotExists, getMyProfile);
 
 // ✅ Route protégée pour mettre à jour son profil
-router.put('/me', updateMyProfile);
+router.put('/me', jwtCheck, injectUser, createUserIfNotExists, updateMyProfile);
 
 // ✅ Route protégée pour uploader un avatar
-router.put('/me/avatar', upload.single("avatar"), uploadAvatar);
+router.put('/me/avatar', jwtCheck, injectUser, createUserIfNotExists, upload.single("avatar"), uploadAvatar);
 
 // ✅ Route protégée pour supprimer un avatar
-router.delete('/me/avatar', deleteAvatar);
+router.delete('/me/avatar', jwtCheck, injectUser, createUserIfNotExists, deleteAvatar);
 
 module.exports = router;
