6c0c6a1b54fb7f58100566538d685f9122c7d25a
Amir-Afkir <afkir202@gmail.com>
2025-06-29 02:24:51 +0200

    pwa

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,39 @@ import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Riveltime',
+        short_name: 'Riveltime',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#f44357',
+        theme_color: '#f44357',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@shared': resolve(__dirname, 'shared'),
