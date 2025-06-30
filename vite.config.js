1f367ac19d27e3cdc02ce42cf8da6353d6660483
Amir-Afkir <afkir202@gmail.com>
2025-06-30 23:15:17 +0200

    ui/ux

--- vite.config.js
+++ vite.config.js
@@ -26,8 +26,8 @@ export default defineConfig({
         start_url: '/',
         scope: '/',
         display: 'standalone', 
+        theme_color: '#ed354f', 
         background_color: '#ffe4e6', 
-        theme_color: '#ed354f',       
         orientation: 'portrait',
         icons: [
           {
