0c8a5bfbcbefe07ef3a62730c49e74b10a274db5
Amir-Afkir <afkir202@gmail.com>
2025-07-13 20:37:26 +0200

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
-import './pwa'; // 👈 ajoute ceci
+//import './pwa'; // 👈 ajoute ceci
 
 import App from './App.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
