cd9f6d44c69e49503be0207f33564b599885e802
Amir-Afkir <afkir202@gmail.com>
2025-07-22 18:06:21 +0200

    Recherche annonces

--- src/pages/livreur/Courses.jsx
+++ src/pages/livreur/Courses.jsx
@@ -177,7 +177,7 @@ useEffect(() => {
                   setAdresseSuggestions([]);
                 }
               }}
-              className="w-full pl-10 pr-4 py-2 text-[15px] border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-[#ed354f] bg-white"
+              className="w-full pl-10 pr-4 py-2 text-[16px] border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-[#ed354f] bg-white"
               autoComplete="off"
               ref={adresseInputRef}
             />
