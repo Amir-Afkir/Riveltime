7a6403e100a0af0e5263ee7b578c872b0d29c18c
Amir-Afkir <afkir202@gmail.com>
2025-07-30 21:27:21 +0200

    pwa

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
         'icon-512.png'
       ],
       manifest: {
+        id: '/', // ✅ requis pour PWABuilder
         name: 'Riveltime',
         short_name: 'Riveltime',
         description: 'Livraison locale rapide et responsable',
@@ -32,11 +33,66 @@ export default defineConfig({
         theme_color: '#ed354f',
         background_color: '#f3f4f6',
         orientation: 'portrait',
+        lang: 'fr',
         icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: 'apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
-        ]
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+          }
+        ],
+        screenshots: [ // ✅ pour PWABuilder + Store publication
+          {
+            src: 'screenshot1.png',
+            sizes: '1280x720',
+            type: 'image/png',
+            form_factor: 'wide'
+          },
+          {
+            src: 'screenshot2.jpeg',
+            sizes: '709x1536',
+            type: 'image/png',
+            form_factor: 'narrow'
+          },
+          {
+            src: 'screenshot3.jpeg',
+            sizes: '709x1536',
+            type: 'image/png',
+            form_factor: 'narrow'
+          },
+          {
+            src: 'screenshot4.jpeg',
+            sizes: '709x1536',
+            type: 'image/png',
+            form_factor: 'narrow'
+          },
+          {
+            src: 'screenshot5.jpeg',
+            sizes: '709x1536',
+            type: 'image/png',
+            form_factor: 'narrow'
+          },
+        ],
+        launch_handler: { // ✅ conseillé pour apparence native
+          client_mode: 'auto'
+        },
+        categories: ['shopping', 'utilities', 'food'], // ✅ conseillé
+        display_override: ['standalone', 'browser'], // ✅ conseil Chrome
+        edge_side_panel: {
+          preferred: true // ✅ pour side panel Windows/Edge
+        }
       },
       cleanupOutdatedCaches: true,
       enableWorkboxModulesLogs: false,
